393906628ac540b750405a13141bb555259bb1c8
SopiaRefaldi <2110131110004@mhs.ulm.ac.id>
2025-02-18 04:01:07 +0800

    update reset

--- backend/config/LOGIN/Database.js
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from "sequelize";
-
-const db = new Sequelize('Csharp_db', 'root', '',{
-    host: "localhost",
-    dialect: "mysql"
-});
-
-export default db;
--- backend/routes/Login/AuthRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import {Login, logOut, Me} from "../../controller/LOGIN/Auth.js"
-const router = express.Router();
-
-router.get('/me', Me);
-router.post('/login', Login);
-router.delete('/logout', logOut);
-export default router;
